be4b0d0c57b3932d6544910713be97af68116a25
khushhallchandra <kcm00000@gmail.com>
2018-01-08 00:55:02 +0530

    Add gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='pytorch-rl',
+      version='0.0.0',
+      description='Reinforcement Learning using PyTorch',
+      author='Khushhall Chandra Mahajan, Pulkit Katdare ',
+      author_email='kcm00000@gmail.com, radiantpulkit@gmail.com',
+      url='https://github.com/khushhallchandra/pytorch-rl',
+      license='MIT',
+      install_requires=['numpy'],
+      packages=find_packages())
